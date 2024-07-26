8cea94d1eca79562a9dace55e3be530a11156049
Mahdiyar733 <makhti733@gmail.com>
2024-07-26 11:52:06 +0330

    first

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				Inter: ["Inter", "sans"],
+				Poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				BG: "#0B132B",
+				WHITE: "#F4D8CD",
+				RED: "#E92B2B",
+				ORNG: "#F97A1F",
+				GRAY: "#D9D9D9",
+				YELLOW: "#EEF226",
+				GREEN: "#37FF33",
+			},
+		},
+	},
+	plugins: [require("daisyui"), require("tailwindcss-animated")],
+};
